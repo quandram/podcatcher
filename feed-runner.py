b1d14b10b3e1afd1947e09ddf2006dac37c6fae7
quandram <quandram@noreply.github.com>
2020-10-24 20:20:13 +0100

    function renames and comment changes

--- feed-runner.py
+++ feed-runner.py
@@ -4,7 +4,7 @@ import os
 from podcatcher import podcatcher
 import configKeys
 
-def UpdateLastProcessed(config, configSection, lastDownloadedDate):
+def update_last_processed_date(config, configSection, lastDownloadedDate):
     config.set(configSection, configKeys.LAST_DOWNLOADED_DATE, lastDownloadedDate.strftime("%Y-%m-%d %H:%M:%S %Z"))
     with open(os.path.join(os.path.dirname(__file__), "config.ini"), "w") as configFile:
         config.write(configFile)
@@ -15,7 +15,7 @@ def main():
 
     for configSection in config.sections():
         if configSection != configKeys.SETTINGS_NAME:
-            UpdateLastProcessed(config, configSection, podcatcher(config[configKeys.SETTINGS_NAME], configSection, config[configSection]).GetNewPods());
+            update_last_processed_date(config, configSection, podcatcher(config[configKeys.SETTINGS_NAME], configSection, config[configSection]).get_new_pods());
 
 if __name__ == "__main__":
     main()
--- podcatcher.py
+++ podcatcher.py
@@ -40,22 +40,22 @@ class podcatcher:
         PACIFIC = tz.gettz("America/Los_Angeles")
         self.timezone_info = {"PST": PACIFIC, "PDT": PACIFIC}
 
-    def GetNewPods(self):
+    def get_new_pods(self):
         feed = feedparser.parse(self.config[configKeys.FEED])
-        lastProcessed = self.GetConfigLastProcessedDate()
+        lastProcessed = self.get_config_last_processed_date()
         podLastDownloaded = lastProcessed
 
-        print ("Downloading %s: " % (self.configSection), end = '')
+        print ("Downloading %s: " % self.configSection, end = '')
         podsDownloaded = 0
         for pod in reversed(feed.entries):
-            podPublishedOn = self.GetUTCDate(pod.published)
+            podPublishedOn = self.get_utc_date(pod.published)
 
             if  podPublishedOn > lastProcessed and (self.maxEpisodesToDownload == 0 or podsDownloaded < self.maxEpisodesToDownload):
                 print (".", end = '')
 
                 try:
                     req = requests.get(pod.links[1]["href"], allow_redirects=True, timeout=(3.05, 27))
-                    open(os.path.join(self.podCatcherConfig[configKeys.OUTPUT], self.configSection, self.GetPodFileName(pod)),
+                    open(os.path.join(self.podCatcherConfig[configKeys.OUTPUT], self.configSection, self.get_pod_file_name(pod)),
                         "wb").write(req.content)
                     podLastDownloaded = podPublishedOn
                     podsDownloaded += 1
@@ -71,21 +71,21 @@ class podcatcher:
                     print(e)
                     break
 
-        print ("\n%s: %d episodes downloaded" % (self.configSection, podsDownloaded ))
+        print (" | %d episodes downloaded\n" % podsDownloaded )
         return podLastDownloaded
 
-    def GetPodFileName(self, pod):
-        podPublishedOn = self.GetUTCDate(pod.published)
-        podExtension = self.GetPodFileExtension(pod)
+    def get_pod_file_name(self, pod):
+        podPublishedOn = self.get_utc_date(pod.published)
+        podExtension = self.get_pod_file_extension(pod)
         if "?" in podExtension:
             podExtension = podExtension.rpartition("?")[0]
         return sanitize(podPublishedOn.strftime("%Y-%m-%dT%H-%M-%SZ") + "_" + self.configSection + "_" + pod.title + "." + podExtension)
 
-    def GetPodFileExtension(self, pod):
+    def get_pod_file_extension(self, pod):
         return pod.links[1]["href"].rpartition(".")[-1]
 
-    def GetUTCDate(self, date):
+    def get_utc_date(self, date):
         return parse(date, tzinfos=self.timezone_info).astimezone(timezone('UTC'))
 
-    def GetConfigLastProcessedDate(self):
-        return self.GetUTCDate(self.config[configKeys.LAST_DOWNLOADED_DATE])
+    def get_config_last_processed_date(self):
+        return self.get_utc_date(self.config[configKeys.LAST_DOWNLOADED_DATE])
